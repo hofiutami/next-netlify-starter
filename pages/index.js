37c8299adf59ba61651ffe26c3e4b0a68a75698f
Hofifa Mulya Utami <49198522+hofiutami@users.noreply.github.com>
2022-01-12 16:34:35 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hofifa Mulya Utami</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my website" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by to know me <code>pages/index.js</code>
         </p>
       </main>
 
